fcfe8589627ca9763330160ec0a66f449894b5dc
Nathan Shanko <natibekele@gmail.com>
2019-08-14 15:18:03 -0400

    clone repo, check manifest

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, abort
+from sh import git
+import os
+import shutil
 
 app = Flask(__name__)
 
@@ -6,7 +9,26 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        return request.json, 200
+        payload = request.json
+        repo_name = payload['repository']['name']
+        repo_clone_url = payload['repository']['clone_url']
+        valid = False
+        
+        git.clone(repo_clone_url)
+       
+        for fname in os.listdir(f'./{repo_name}'):
+            if fname == 'manifest.json' or fname == 'manifest.yaml':
+                valid = True
+                break
+        
+        try: 
+            shutil.rmtree(f'./{repo_name}')
+            
+        except OSError as e:  ## if failed, report it back to the user ##
+             print ("Error: %s - %s." % (e.filename, e.strerror))
+
+
+        return str(valid), 200
     else:
         abort(400)
 
